b54597b143693a5a27967bfbfab9f104b12a64bf
Akjos <sojka_p@o2.pl>
2020-09-01 21:06:39 +0200

    init local repo on github

---
+++ src/main/java/pl/akjos/training_manager/TrainingManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.akjos.training_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingManagerApplication.class, args);
+	}
+
+}
